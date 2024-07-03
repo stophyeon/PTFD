c39a9e678c19a78b84806938d99690a11324cce8
stophyeon <jeongjihyeon401@gmail.com>
2024-07-03 14:05:25 +0900

    Collection 생성, 쿼리문 변경

--- Chatting/src/main/java/org/example/repository/ChatRoomRepository.java
+++ Chatting/src/main/java/org/example/repository/ChatRoomRepository.java
@@ -1,10 +1,10 @@
 package org.example.repository;
 
 import org.example.entity.ChattingRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatRoomRepository extends JpaRepository<ChattingRoom,Long> {
+public interface ChatRoomRepository extends MongoRepository<ChattingRoom,Long> {
     public ChattingRoom findByRoomId(Long roomId);
 }
--- Chatting/src/main/java/org/example/repository/MessageRepository.java
+++ Chatting/src/main/java/org/example/repository/MessageRepository.java
@@ -1,12 +1,12 @@
 package org.example.repository;
 
 import org.example.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message,Long> {
+public interface MessageRepository extends MongoRepository<Message,Long> {
     public List<Message> findAllByRoomId(Long roomId);
 }
