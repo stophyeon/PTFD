d4379b41ba11729ea4fb6d2b44fd4cac1998bcd5
stophyeon <jeongjihyeon401@gmail.com>
2024-07-07 16:29:09 +0900

    채팅 기능 개발

--- Chatting/src/main/java/org/example/controller/ChatController.java
+++ Chatting/src/main/java/org/example/controller/ChatController.java
@@ -2,38 +2,42 @@ package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.dto.MessageDto;
-import org.example.dto.MessageType;
-import org.example.service.ChatService;
+
+
+import org.apache.kafka.common.protocol.Message;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping(value = "/kafka")
 public class ChatController {
-    private final SimpMessagingTemplate simpMessagingTemplate;
-    private final ChatService chatService;
+    private final KafkaTemplate<String, Message> kafkaTemplate;
+    @PostMapping(value = "/publish")
+    public void sendMessage(@RequestBody Message message) {
+        log.info("Produce message : " + message.toString());
 
-    //채팅방애 입장
-    @MessageMapping("/send")
-    public void joinRoom(MessageDto message) {
-        if(MessageType.ENTER.equals(message.getType())){
-            message.setMessage(message.getSender()+"님이 입장했습니다.");
-            log.info(message.getMessage());
+        try {
+            kafkaTemplate.send("chat", message).get();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        chatService.saveMsg(message);
-        log.info(message.getSender());
-        log.info(message.getRoomId().toString());
-        log.info(message.getMessage());
-        simpMessagingTemplate.convertAndSend("/sub/chat/" + message.getRoomId(),message);
+    }
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/group")
+    public Message broadcastGroupMessage(@Payload Message message) {
+        return message;
     }
 
 
